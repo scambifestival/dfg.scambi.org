87043977eb27d4f3101a2be4a86bd4ed0d2a7929
Soma Sai Kumar Badri <sobadri@ucsc.edu>
2022-03-25 21:46:12 -0700

    nlssubscribe first draft

--- src/home-page/NLSubscribe.js
+++ src/home-page/NLSubscribe.js
@@ -10,12 +10,14 @@ class NLSubscribe extends Component {
                 <img src={SubscribeImage} />
             </div>
             <div className='right-container'> 
-                <div>exchanges of letters, our newsletter</div>
-                <h2>Scambi Epistolari</h2>
-                <div>Join the conversation - sign up now to receive updates on Scambi activities.</div>
-                <div>
-                    <TextField placeholder="Email" size={TextField.sizes.LARGE} />
-                    <Button className='submit-button'>Submit</Button>
+               <div className='inner-container'>
+                    <div className='upper-subtitle'>exchanges of letters, our newsletter</div>
+                    <div className='title'>Scambi Epistolari</div>
+                    <div className='lower-subtitle'>Join the conversation - sign up now to receive updates on Scambi activities.</div>
+                    <div className='email-fields'>
+                        <input className='textfield' type="text" placeholder="Email" />
+                        <button className='submit-button'>Submit</button>
+                    </div>
                 </div>
             </div>
         </div>
