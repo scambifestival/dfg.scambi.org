8b9d79df43cb43f56a47809d7fd4f2644aa4bb71
koiralasasbit <koiralasasbit@gmail.com>
2022-04-03 12:22:11 -0400

    lab cards
    created lab cards

--- src/labs-page/LabCard.js
+++ src/labs-page/LabCard.js
@@ -1,9 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ReadMoreReact from "read-more-react";
+import labcard from "../images/labcard.png";
+import "./LabCard.css";
 
 class LabCard extends Component {
-    render() {
-        return <div>Lab Card</div>
-    }
+  state = {
+    lab_datas: {
+      title: "Human Thread Lab",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+    },
+    readmore: <text className="read-more-link">Read more &#10140;</text>,
+  };
+  render() {
+    return (
+      <div className="main-container">
+        <div className="left-container">
+          <div className="title">{this.state.lab_datas.title}</div>
+          <div className="image-container">
+            <img src={labcard} alt="lab1" />
+          </div>
+          <div className="lower-subtitle">
+            <ReadMoreReact
+              text={this.state.lab_datas.description}
+              min={100}
+              ideal={200}
+              max={1000}
+              readMoreText={this.state.readmore}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default LabCard;
--- src/labs-page/Labs.js
+++ src/labs-page/Labs.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import LabCard from "./LabCard";
 class Labs extends Component {
-    render() {
-        return <div>Labs</div>
-    }
+  render() {
+    return (
+      <div>
+        <h1>Lab Cards</h1>
+        <LabCard />
+      </div>
+    );
+  }
 }
 
 export default Labs;
