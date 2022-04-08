a036b8b77eaf2e265e2af31dbfb2552733c03e5f
koiralasasbit <koiralasasbit@gmail.com>
2022-04-07 23:39:38 -0400

    second update

--- src/labs-page/LabCard.js
+++ src/labs-page/LabCard.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import ReadMoreReact from "read-more-react";
-import labcard from "../images/labcard.png";
+import Stroke from "../images/Stroke.png";
 import "./LabCard.css";
 
 class LabCard extends Component {
@@ -10,24 +9,22 @@ class LabCard extends Component {
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
     },
-    readmore: <text className="read-more-link">Read more &#10140;</text>,
   };
   render() {
     return (
       <div className="main-container">
         <div className="left-container">
-          <div className="title">{this.state.lab_datas.title}</div>
-          <div className="image-container">
-            <img src={labcard} alt="lab1" />
-          </div>
-          <div className="lower-subtitle">
-            <ReadMoreReact
-              text={this.state.lab_datas.description}
-              min={100}
-              ideal={200}
-              max={1000}
-              readMoreText={this.state.readmore}
-            />
+          <div className="image-container"></div>
+          <div className="text-container">
+            <div className="link-container">
+              <div className="readmore">
+                <a>Read more &#10140;</a>
+              </div>
+            </div>
+            <div className="title">{this.state.lab_datas.title}</div>
+            <div className="lower-subtitle">
+              {this.state.lab_datas.description}
+            </div>
           </div>
         </div>
       </div>
