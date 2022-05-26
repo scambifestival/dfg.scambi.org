d8c2f7607837d3edc6df8a9f937b0d1faa633f7c
PaulRamos007 <pramos01@mynbcc.ca>
2022-05-26 10:49:25 -0300

    Updated
    Need BG image from figma. Needs to be responsive as well.

--- src/home-page/Home.js
+++ src/home-page/Home.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
-import UserTest from './UserTest';
-import NLSubscribe from './NLSubscribe';
+import React, { Component } from "react";
+import UserTest from "./UserTest";
+import Family from "./family";
 
 class Home extends Component {
-    render() {
-        return <div>
-            <div>Home</div>
-            <UserTest />
-            <NLSubscribe />
-        </div>
-    }
+  render() {
+    return (
+      <div>
+        <div>Home</div>
+        <UserTest />
+        <Family />
+      </div>
+    );
+  }
 }
 
 export default Home;
