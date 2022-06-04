836ebf740273617beb47306ec85ca5f6e53aef72
PaulRamos007 <pramos01@mynbcc.ca>
2022-06-04 13:33:10 -0300

    manifesto/values done

--- src/home-page/Home.js
+++ src/home-page/Home.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
-import UserTest from './UserTest';
-import NLSubscribe from './NLSubscribe';
+import React, { Component } from "react";
+import UserTest from "./UserTest";
+import Values from "../values-page/Values";
 
 class Home extends Component {
-    render() {
-        return <div>
-            <div>Home</div>
-            <UserTest />
-            <NLSubscribe />
-        </div>
-    }
+  render() {
+    return (
+      <div>
+        <div>Home</div>
+        <UserTest />
+        <Values />
+      </div>
+    );
+  }
 }
 
 export default Home;
