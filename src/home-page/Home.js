21009b3e8496632f91bb36728ace6cc4184b894e
PaulRamos007 <pramos01@mynbcc.ca>
2022-06-04 14:15:06 -0300

    updated partners

--- src/home-page/Home.js
+++ src/home-page/Home.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
-import UserTest from './UserTest';
-import NLSubscribe from './NLSubscribe';
+import React, { Component } from "react";
+import UserTest from "./UserTest";
+import Partners from "../partners-page/partners";
 
 class Home extends Component {
-    render() {
-        return <div>
-            <div>Home</div>
-            <UserTest />
-            <NLSubscribe />
-        </div>
-    }
+  render() {
+    return (
+      <div>
+        <div>Home</div>
+        <UserTest />
+        <Partners />
+      </div>
+    );
+  }
 }
 
 export default Home;
