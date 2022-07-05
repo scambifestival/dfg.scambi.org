44ab1c223f01195098c981e64a6755d634f0fb6f
Rion Sawabe <rose1029ri@yahoo.co.jp>
2022-07-04 23:34:07 -0700

    Created flex component

--- src/pages/about/AboutHeader.js
+++ src/pages/about/AboutHeader.js
@@ -1,29 +1,15 @@
 import React, { Component } from "react";
-import "./About.css";
+import AboutHeaderText from "./AboutHeaderText";
+import Flex from "../../components/Flex/Flex";
+import "../../components/Flex/Flex.css";
 
 class AboutHeader extends Component {
 	render() {
 		return (
-			<div className="aboutScambiHeader-container">
-				<div className="left-aboutScambi">
-					<h1 className="scambi-text">About Scambi</h1>
-					<p>
-						Scambi, mearninig "exchange" in Italian, is the festival of
-						interactive workshop organized in Pigna, the old town of Sanremo
-						(italy) by a team of forty young people under-25,from all around
-						Europe.
-					</p>
-					<p>
-						Join us for our second year in Sanremo Italy on Aug 25 - 28, 2022,
-						where we will explore{" "}
-						<span className="disequilibirum">Disequilibirum.</span>
-					</p>
-					<button className="attendButton">Attend</button>
-				</div>
-				<div className="right-aboutScambi">
-					<img src="SubscribeImage.png" />
-				</div>
-			</div>
+			<Flex>
+				<AboutHeaderText />
+				<img src="#" />
+			</Flex>
 		);
 	}
 }
--- src/pages/about/AboutWorkshopExplanation.js
+++ src/pages/about/AboutWorkshopExplanation.js
@@ -1,34 +1,15 @@
 import React, { Component } from "react";
-import "./About.css";
+import AboutWorkShopText from "./AboutWorkShopText";
+import Flex from "../../components/Flex/Flex";
+import "../../components/Flex/Flex.css";
 
 class AboutWorkshopExplanation extends Component {
 	render() {
 		return (
-			<div className="aboutScambiExplanation-container">
-				<div className="left-aboutScambi">
-					<img src="SubscribeImage.png" />
-				</div>
-				<div className="right-aboutScambi-workExplanation">
-					<h6 className="scambiExplanation-text">
-						Our purpose is to recover the value of{" "}
-						<span className="purpleText">communication</span> and{" "}
-						<span className="purpleText">meeting</span> through paneuretic
-						workshops
-					</h6>
-					<p className="scambi-workExplanation-text">
-						Create your personalized journey of workshops from topics moving
-						from physics to languages, from history to dancing, from music to
-						law, from cooking to activism.
-					</p>
-					<p className="scambi-workExplanation-text">
-						All workshops will be instrinsically careful to issues like
-						<span className="bold-fonts">
-							accessibility, sustainebility, and inclusion.
-						</span>
-					</p>
-					<button className="exploreWorkshopButton">Explore workshops</button>
-				</div>
-			</div>
+			<Flex>
+				<img src="#" />
+				<AboutWorkShopText />
+			</Flex>
 		);
 	}
 }
