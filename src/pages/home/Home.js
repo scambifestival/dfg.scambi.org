668c6623409fce3b249262f7988e28a5a536121c
Uyen Hoang <minhuyen0512@gmail.com>
2022-07-10 15:39:25 -0700

    Fix some typo

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,12 @@
 import UserTest from './UserTest';
 import NLSubscribe from './NLSubscribe';
+import Button from '../../components/button';
 
 export default function Home() {
 	return (
 		<div>
 			<p>Home</p>
+			<Button>Hi</Button>
 		</div>
 	);
 }
