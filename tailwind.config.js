c1eb4b25f1e10f1ff8140556357729d0ec8fce93
traciingu <traciinguyen@hotmail.com>
2022-07-09 23:12:28 -0400

    Created reusable button component with responsive states

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
 				'3xl': '0 5px 3px 0px rgb(0 0 0 / 0.5)',
 				'yellow-3xl': '0 5px 4px 0px rgb(155 149 6 / 0.7)'
 			},
+			borderWidth: {
+				'3': '3px'
+			}
 		},
 	},
 	plugins: [],
