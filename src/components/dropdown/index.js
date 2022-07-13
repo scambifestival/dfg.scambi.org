9597c20040a5e04ba5a1ac68d2a7f903342ff4ea
traciingu <traciinguyen@hotmail.com>
2022-07-13 15:59:54 -0400

    Added open/close dropdown functionality

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -1,11 +1,27 @@
+import { useRef, useState } from 'react';
+import './Dropdown.css';
+
 export default function Dropdown({
 	btnStyle = "",
 	dropdownStyle = "",
 	content,
-	children,
+	children
 }) {
+	const menu = useRef(null);
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleClick = () => {
+		setIsOpen(!isOpen);
+	};
+
+	const handleBlur = (e) => {
+		if (!menu.current.contains(e.relatedTarget)) {
+			setIsOpen(false);
+		}
+	};
+
 	return (
-		<li className='relative inline-block text-left'>
+		<li className='relative inline-block text-left' role='none' key={content} onClick={handleClick} ref={menu} onBlur={handleBlur}>
 			<a
 				role="menuitem"
 				href="#"
@@ -14,19 +30,26 @@ export default function Dropdown({
 				aria-haspopup="true"
 			>
 				{content}
-				<span className="pl-2">
+				<span className="pl-2" aria-hidden="true">
 					<i className="fa fa-chevron-down" />
 				</span>
 			</a>
-			<ul className={`${dropdownStyle} py-1 origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none`}
+			<ul
+				className={`${dropdownStyle} ${isOpen ? "open" : "close"} py-1 origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none`}
+				id={`sub-menu-${content.toLowerCase()}`}
 				role="menu"
+				aria-label={content}
 				aria-orientation="vertical"
 				aria-labelledby="menu-button"
-				tabindex="-1"
 			>
-				{children.map(child => (
-					<li>
-						<a href={`/${child.href}`} class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">{child.title}</a>
+				{children.map((child, index) => (
+					<li key={`${content}-${index}`}>
+						<a href={`#${child.href}`}
+							className="text-gray-700 block px-4 py-2 text-sm"
+							role="menuitem"
+						>
+							{child.title}
+						</a>
 					</li>
 				))}
 			</ul>
