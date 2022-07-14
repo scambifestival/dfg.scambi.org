d4f2040fc58c2195ba54ac54b081eecea4ad829f
traciingu <traciinguyen@hotmail.com>
2022-07-14 12:33:15 -0400

    Refactored homepage header with Tailwind

--- src/pages/home/Header.js
+++ src/pages/home/Header.js
@@ -1,34 +1,24 @@
-import React, { Component } from 'react';
-import './Header.scss';
+import React from 'react';
 import ReactPlayer from 'react-player'
 
-class Header extends Component {
-    calcWidth(){
-        let width = `"${window.innerWidth}px"`
-        console.log(width)
-        return "1000px"
-    }
-
-    render() {
-        return <div>
-            {/* bold communication and meeting */}
-            {/* spacing in navbar */}
-            {/* add support to navbar */}
-            <div className='header-top-container'>
-                <div className='header-left-container'>
-                    <div className="header-title">Scambi Festival</div>
-                    <div className="header-subtitle">The Festival of Paneuretic Workshops, free for all to attend</div>
+const Header = () => {
+    return (
+        <div>
+            <div className='w-10/12 mx-auto my-10 text-left flex flex-col justify-between xl:items-center  xl:flex-row'>
+                <div>
+                    <h1 className='leading-none'>Scambi Festival</h1>
+                    <h3>The Festival of Paneuretic Workshops, free for all to attend</h3>
                 </div>
-                <div className='header-right-container'>
-                    August 25 - 28, 2022 <br/>
-                    La Pigna, Sanremo, Italy
-                </div>
+                <p className='font-semibold'>
+                    <span className='text-2xl'>August 25 - 28, 2022</span> <br />
+                    <span className='text-lg'>La Pigna, Sanremo, Italy</span>
+                </p>
             </div>
-            <div className="header-video">
+            <div className='min-h-[20rem] mx-5 flex justify-center md:min-h-[32rem] md:mx-10 md:mt-10 xl:mt-16'>
                 <ReactPlayer width='100%' height='auto' url='https://vimeo.com/658183199' />
             </div>
         </div>
-    }
-}
+    );
+};
 
 export default Header;
