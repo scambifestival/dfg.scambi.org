b41a3a69f1a39ff31833f58ad5cc1052ddc4466b
Rion Sawabe <rose1029ri@yahoo.co.jp>
2022-07-14 14:34:45 -0700

    small adjustment

---
+++ src/components/button/index.js
@@ -0,0 +1,18 @@
+export default function Button({
+	children,
+	href = "",
+	styleType = "primary",
+	type = "button",
+	classes = ""
+}) {
+	const buttonStyle = "btn-" + styleType;
+
+	return (
+		<button
+			type={type}
+			className={`rounded-2xl font-semibold ${classes} ${buttonStyle} regular`}
+			onClick={() => (window.location.href = href)}>
+			{children}
+		</button>
+	);
+}
