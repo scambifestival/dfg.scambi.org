4b13c379478f94ad6478f40640279c0eac074e32
traciingu <traciinguyen@hotmail.com>
2022-07-15 18:11:13 -0400

    Create Pinoli cards and populate page

--- src/pages/minivents/MiniVents.js
+++ src/pages/minivents/MiniVents.js
@@ -1,8 +1,42 @@
 import Pinoli from '../../assets/minievent/Pinoli.png';
 import Flex from '../../components/Flex/Flex';
+import Card from '../../components/card';
 import EventPlaceholder from '../../assets/minievent/EventPlaceholder.jpg';
 
+const dummyData = {
+    title: "Event name",
+    // image: {EventPlaceholder},
+    organization: "Organization",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut diam lorem nec amet sit morbi tortor ullamcorper quis.",
+    link: "#"
+};
+
 const MiniEvents = () => {
+    const populate = () => {
+        const events = [];
+
+        for (let i = 0; i < 9; i++) {
+            events.push(
+                <Card classes='bg-white rounded-xl text-left px-6 py-7 space-y-4 shadow-2xl h-fit mx-7 my-5 max-w-full md:max-w-xs lg:max-w-sm'>
+                    <img
+                        src={EventPlaceholder}
+                        className='rounded-xl object-cover object-right w-full h-2/5'
+                        alt='Woman reading from phone with microphone while another woman signs what is being read'
+                    />
+                    <h4 className='font-["Poppins"] font-semibold'>{dummyData.title}</h4>
+                    <h5 className='font-["Poppins"] font-semibold'>{dummyData.organization}</h5>
+                    <p>{dummyData.description}</p>
+                    <p>
+                        <a className='text-purple-650' href={`/${dummyData.link}`}>
+                            Read more <i className='fa fa-long-arrow-right' />
+                        </a>
+                    </p>
+                </Card>
+            );
+        }
+
+        return events;
+    };
         
     return (
         <div>
@@ -31,6 +65,10 @@ const MiniEvents = () => {
             <div className='px-20 my-10'>
                 <h2 className='pt-5'>Upcoming Pinolis</h2>
                 {/* <Map/> */}
+            </div>
+
+            <div className='flex flex-wrap justify-center mx-10 lg:mx-20'>
+                {populate()}
             </div>
         </div>
     );
