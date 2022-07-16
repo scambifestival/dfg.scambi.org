5c00716cf307d988fdb1a0c8f3fc1024a2e0ee78
Rion Sawabe <rose1029ri@yahoo.co.jp>
2022-07-15 21:28:47 -0700

    refactored ourRelation & manifesto

--- src/components/OurManifesto/OurManifesto.js
+++ src/components/OurManifesto/OurManifesto.js
@@ -1,59 +1,26 @@
 import React, { Component } from "react";
-import Card from "./../../components/card/index";
-import Flex from "../Flex/Flex";
-import "./OurManifesto.css";
+import Card from "../../components/card/index";
+import { manifestoCards } from "./../../pages/home/data";
+import Button from "../button/index";
 
 class OurManifesto extends Component {
 	render() {
 		return (
-			<div className="pt-36">
+			<div className="my-20 space-y-12">
 				<h2>Our Manifesto</h2>
-				<Flex classes="cardContainer w-max p-0">
-					<Card classes="p-4 lg:w-56 md:w-56 sm:w-96 m-1 cardEl">
-						<p className="mb-8">
-							<b>Workshop</b>
-						</p>
-						<p>
-							Collective experience based on creativity, dialogue and sharing,
-							which blossoms once a year on the squares of La Pigna in order to
-							explore together the various connotations of the current edition's
-							theme.
-						</p>
-					</Card>
-					<Card classes="bg-white p-4 lg:w-56 md:w-56 sm:w-96 m-4 cardEl">
-						<p className="mb-8">
-							<b>Paneuretic</b>
-						</p>
-						<p>
-							Neologism deriving from the Greek pan (all), eurisko (discover)
-							and ethos (living), because we wish to combine diverse realities
-							that would never meet in their original contexts, while
-							celebrating and respecting our differences, discovering and
-							developing inspiring ideas.
-						</p>
-					</Card>
-					<Card classes="p-4 lg:w-56 md:w-56 sm:w-96 m-4 cardEl">
-						<p className="mb-8">
-							<b>Accessibility</b>
-						</p>
-						<p>
-							In a society that says "you must", we say "just be": we made every
-							festival activity available and accessible to as many people as
-							possible.
-						</p>
-					</Card>
-					<Card classes="bg-white p-4 lg:w-56 md:w-56 sm:w-96 m-4 cardEl">
-						<p className="mb-8">
-							<b>La Pigna</b>
-						</p>
-						<p>
-							The iconic and vibrant historic center of Sanremo that welcomes us
-							during the festival, and that we are commited to requalify through
-							social values such as Community and Meeting.
-						</p>
-					</Card>
-				</Flex>
-				<button className="manifestoBtn">Read the Manifesto</button>
+				<div className="flex flex-col justify-center mx-16 space-y-6 lg:space-x-5 lg:space-y-0 lg:flex-row xl:space-x-10 2xl:mx-32">
+					{manifestoCards.map((card, index) => (
+						<Card
+							classes={`p-5 w-full h-fit lg:w-1/4 xl:w-1/5 ${
+								index % 2 === 0 ? "yellow-pink-gradient" : "bg-white"
+							}`}
+							key={`manifesto-${index}`}>
+							<h3 className="leading-loose">{card.title}</h3>
+							<p className="leading-relaxed">{card.description}</p>
+						</Card>
+					))}
+				</div>
+				<Button styleType="light">Read the Manifesto</Button>
 			</div>
 		);
 	}
--- src/components/OurRelations/OurRelations.js
+++ src/components/OurRelations/OurRelations.js
@@ -1,21 +1,29 @@
 import React, { Component } from "react";
 import Flex from "../Flex/Flex";
+import Button from "../button/index";
 import ourRelationPics from "./../../assets/ourRelations/Sponsors.png";
-import RelationsText from "./RelationsText";
 
 class OurRelations extends Component {
 	render() {
 		return (
-			<Flex className="flex-row-reverse">
-				<RelationsText />
-				<div className="lg:w-1/2 md:w-1/2 w-1/2 lg:mr-14 md:mr-14">
-					<img
-						src={ourRelationPics}
-						className="borderImg"
-						width="400px"
-						height="100px"
-						alt=""
-					/>
+			<Flex
+				classes="my-1 px-8 space-y-10 md:space-x-reverse xl:gap-x-52"
+				reverse={true}>
+				<div className="w-4/5 flex flex-col text-center space-y-8 md:text-left">
+					<h2 className="leading-tight lg:leading-normal">Our Relations</h2>
+					<p className="mx-auto md:mx-0 w-full 2xl:w-4/5">
+						Alone we would not have been able to concretize the magic of Scambi.
+						What makes our festival unique are the relationships and the bonds
+						with local and non-local bodies, the public administration of
+						Sanremo and many other special people.
+					</p>
+					<div className="flex flex-row justify-center md:justify-start space-x-7">
+						<Button>Partner with us</Button>
+						<Button styleType="secondary">Meet our partners</Button>
+					</div>
+				</div>
+				<div className="w-3/5 lg:w-4/12">
+					<img src={ourRelationPics} alt="" />
 				</div>
 			</Flex>
 		);
