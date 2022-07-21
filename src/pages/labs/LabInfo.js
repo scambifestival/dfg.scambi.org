45c2c32a21611dd6a479b71188651182cb34cac6
Uyen Hoang <minhuyen0512@gmail.com>
2022-07-21 10:07:11 -0700

    Make it more mobile responsive

--- src/pages/labs/LabInfo.js
+++ src/pages/labs/LabInfo.js
@@ -43,7 +43,38 @@ function LabInfo() {
 							eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus
 							in mollis nunc sed. Et molestie ac feugiat sed lectus.{' '}
 						</p>
-						<p>
+						<div className='flex justify-center'>
+							<svg
+								className='lg:hidden'
+								width='242'
+								height='317'
+								viewBox='0 0 242 317'
+								fill='none'
+								xmlns='http://www.w3.org/2000/svg'>
+								<defs>
+									<pattern
+										id='img1'
+										patternUnits='userSpaceOnUse'
+										width='242'
+										height='317'>
+										<image
+											href='https://picsum.photos/242/317'
+											x='0'
+											y='0'
+											width='242'
+											height='317'
+										/>
+									</pattern>
+								</defs>
+								<path
+									fillRule='evenodd'
+									clipRule='evenodd'
+									d='M226.959 24.4434C243.794 49.2094 235.86 97.0334 236.441 137.812C237.021 178.377 245.729 211.896 238.763 244.562C231.603 277.014 208.383 308.398 180.519 315.23C152.655 322.062 119.954 304.555 89.768 288.329C59.582 272.103 32.105 257.372 17.012 232.819C1.919 208.053 -0.983507 173.466 1.91899 137.385C4.62799 101.09 13.3355 63.5139 35.201 39.6019C57.0665 15.9034 92.2835 6.29595 130.596 2.02595C168.909 -2.03055 210.318 -0.322548 226.959 24.4434Z'
+									fill='url(#img1)'
+								/>
+							</svg>
+						</div>
+						<p className='lg:mt-0 mt-5'>
 							Libero volutpat sed cras ornare. Nulla facilisi nullam vehicula
 							ipsum a arcu cursus vitae. Magna fringilla urna porttitor rhoncus
 							dolor purus non enim. Porta nibh venenatis cras sed felis eget
@@ -53,40 +84,43 @@ function LabInfo() {
 							amet mattis vulputate.{' '}
 						</p>
 					</div>
-					<svg
-						width='242'
-						height='317'
-						viewBox='0 0 242 317'
-						fill='none'
-						xmlns='http://www.w3.org/2000/svg'>
-						<defs>
-							<pattern
-								id='img'
-								patternUnits='userSpaceOnUse'
-								width='242'
-								height='317'>
-								<image
-									href='https://picsum.photos/242/317'
-									x='0'
-									y='0'
+					<div className='hidden lg:block'>
+						<svg
+							className=''
+							width='242'
+							height='317'
+							viewBox='0 0 242 317'
+							fill='none'
+							xmlns='http://www.w3.org/2000/svg'>
+							<defs>
+								<pattern
+									id='img'
+									patternUnits='userSpaceOnUse'
 									width='242'
-									height='317'
-								/>
-							</pattern>
-						</defs>
-						<path
-							fillRule='evenodd'
-							clipRule='evenodd'
-							d='M226.959 24.4434C243.794 49.2094 235.86 97.0334 236.441 137.812C237.021 178.377 245.729 211.896 238.763 244.562C231.603 277.014 208.383 308.398 180.519 315.23C152.655 322.062 119.954 304.555 89.768 288.329C59.582 272.103 32.105 257.372 17.012 232.819C1.919 208.053 -0.983507 173.466 1.91899 137.385C4.62799 101.09 13.3355 63.5139 35.201 39.6019C57.0665 15.9034 92.2835 6.29595 130.596 2.02595C168.909 -2.03055 210.318 -0.322548 226.959 24.4434Z'
-							fill='url(#img)'
-						/>
-					</svg>
+									height='317'>
+									<image
+										href='https://picsum.photos/242/317'
+										x='0'
+										y='0'
+										width='242'
+										height='317'
+									/>
+								</pattern>
+							</defs>
+							<path
+								fillRule='evenodd'
+								clipRule='evenodd'
+								d='M226.959 24.4434C243.794 49.2094 235.86 97.0334 236.441 137.812C237.021 178.377 245.729 211.896 238.763 244.562C231.603 277.014 208.383 308.398 180.519 315.23C152.655 322.062 119.954 304.555 89.768 288.329C59.582 272.103 32.105 257.372 17.012 232.819C1.919 208.053 -0.983507 173.466 1.91899 137.385C4.62799 101.09 13.3355 63.5139 35.201 39.6019C57.0665 15.9034 92.2835 6.29595 130.596 2.02595C168.909 -2.03055 210.318 -0.322548 226.959 24.4434Z'
+								fill='url(#img)'
+							/>
+						</svg>
+					</div>
 				</div>
 			</div>
 
 			<div className='p-10 mt-10'>
 				<h3 className='mb-10 text-left'>Similar Labs</h3>
-				<div className='grid lg:grid-cols-2 lg:gap-x-5 xl:gap-x-5'>
+				<div className='grid grid-cols-1 gap-y-5 lg:grid-cols-2 lg:gap-x-5 xl:gap-x-5'>
 					<LabCard
 						title='Lorem Ipsum'
 						description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
