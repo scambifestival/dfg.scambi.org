f202f410dff4af415e7e07313620e0670234b102
traciingu <traciinguyen@hotmail.com>
2022-07-21 15:09:46 -0400

    Restyle dropdown for better visibility and accessibility

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -1,6 +1,5 @@
 import { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
-//import { HashLink } from 'react-router-hash-link';
 
 export default function Dropdown({
 	btnStyle,
@@ -25,7 +24,7 @@ export default function Dropdown({
 		<div className='relative inline-block' onBlur={clickOutside}>
 			<button
 				onClick={handleClick}
-				className={`${btnStyle} inline-flex justify-center px-4 py-2 bg-white focus:outline-none active:ring-2 active:ring-fuchsia-800`}>
+				className={`${btnStyle} inline-flex justify-center px-4 py-2 bg-white active:ring-2 active:ring-fuchsia-800`}>
 				{content}
 				<span className='pl-2' aria-hidden='true'>
 					<i className='fa fa-chevron-down' />
@@ -35,7 +34,7 @@ export default function Dropdown({
 				ref={menu}
 				className={`${dropdownStyle} ${
 					isOpen ? '' : 'hidden'
-				} py-1 left-0 mt-2 w-56 rounded-md bg-white ring-1 ring-black ring-opacity-5 lg:absolute lg:shadow-lg`}
+				} py-1 left-0 mt-2 w-56 rounded-md bg-white ring-1 ring-black ring-opacity-5 lg:absolute lg:shadow-xl`}
 				id={content}
 				role='menu'
 				aria-label={content}
@@ -45,7 +44,7 @@ export default function Dropdown({
 					<li key={index}>
 						<Link
 							to={child.href}
-							className='text-lg font-bold block px-4 py-2 text-sm uppercase'
+							className='text-lg font-bold block px-4 py-2 uppercase'
 							role='menuitem'>
 							{child.title}
 						</Link>
