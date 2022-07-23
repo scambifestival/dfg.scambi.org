dcb938df82d95964f7e241c4c76bbe673e128632
Rion Sawabe <rose1029ri@yahoo.co.jp>
2022-07-23 10:44:36 -0700

    small adjustment

--- src/pages/footer/ FooterTop/FooterTop.js
+++ src/pages/footer/ FooterTop/FooterTop.js
@@ -8,11 +8,11 @@ const FooterTop = () => {
 			<div className="topLeft w-1/5">
 				<h2 className="headText pl-16 pb-0 pt-4">Scambi</h2>
 			</div>
-			<div className="topRight flex p-4">
-				<div className="topRigh1t w-2/3 p-3">
+			<div className="topRight flex pt-4">
+				<div className="topRigh1t w-2/3 p-8">
 					<h6 className="readyToJoin">Ready to join us?</h6>
 				</div>
-				<div className="topRight2">
+				<div className="topRight2  pt-4">
 					<Button>ATTEND</Button>
 				</div>
 			</div>
