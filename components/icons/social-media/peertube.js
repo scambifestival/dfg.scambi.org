becaf2c075d289f07872d188e5f724a42078e5d3
Uyen Hoang <minhuyen0512@gmail.com>
2022-07-25 00:07:25 -0700

    Finish adding components, Homepage, and About page

---
+++ components/icons/social-media/peertube.js
@@ -0,0 +1,15 @@
+export default function Peertube() {
+	return (
+		<svg
+			width='24'
+			height='24'
+			viewBox='0 0 24 24'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'>
+			<path
+				d='M12 6.545V17.455L20.727 12L12 6.545ZM3.27301 12V24L12 17.455L3.27301 12ZM3.27301 0V12L12 6.545'
+				fill='#69008D'
+			/>
+		</svg>
+	);
+}
