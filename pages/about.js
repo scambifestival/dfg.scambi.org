62c8264e98a9210c5327d1c6e8048c3cf8ee0df2
Uyen Hoang <minhuyen0512@gmail.com>
2022-07-26 16:11:47 -0700

    Our Teams page and Team inidividual page.

--- pages/about.js
+++ pages/about.js
@@ -4,6 +4,7 @@ import Card from '../components/card';
 import Image from 'next/image';
 import { manifestoCards } from '../assets/data';
 import Subscribe from '../components/subscribe';
+import SupportUs from '../components/support-cards';
 
 export default function About() {
 	return (
@@ -415,52 +416,7 @@ export default function About() {
 				/>
 			</Flex>
 			<Subscribe />
-			<div className='my-40'>
-				<h2 className='text-center mb-10'>Support Us Today</h2>
-				<div className='grid grid-cols-1 lg:grid-cols-3 lg:gap-x-10 px-10'>
-					<Card classes='bg-[#EEBDAD] text-center py-10 space-y-5'>
-						<h4>Volunteer with us</h4>
-						<p>Join us in the festivities and help from the inside!</p>
-						<div>
-							<Image
-								src='/illustrations/support1.png'
-								width={32}
-								height={88}
-								alt=''
-							/>
-						</div>
-						<Button>Volunteer</Button>
-					</Card>
-					<Card classes='bg-[#EDBBFF] text-center py-10 space-y-5'>
-						<h4>Join us Aug 25 - 28, 2022</h4>
-						<p>Celebrate with us this year, tickets are free of charge.</p>
-						<div>
-							<Image
-								src='/illustrations/support2.png'
-								width={32}
-								height={88}
-								alt=''
-							/>
-						</div>
-						<Button>Get Tickets</Button>
-					</Card>
-					<Card classes='bg-[#AAB4D6] text-center py-10 space-y-5'>
-						<h4>Donate Securely</h4>
-						<p>
-							Make a one-time or monthly donation to help run Scambi Festival.
-						</p>
-						<div>
-							<Image
-								src='/illustrations/support3.png'
-								width={32}
-								height={88}
-								alt=''
-							/>
-						</div>
-						<Button>Donate</Button>
-					</Card>
-				</div>
-			</div>
+			<SupportUs />
 		</section>
 	);
 }
--- pages/teams/index.js
+++ pages/teams/index.js
@@ -1,7 +1,67 @@
-export default function Teams() {
+import Button from '../../components/button';
+import Flex from '../../components/flex';
+import Image from 'next/image';
+import { TeamCard } from '../../components/card/teams-card';
+import sample from '../../public/illustrations/pinoli.png';
+import { getAllTeams, getATeam } from '../../lib/teams';
+import fs from 'fs';
+import matter from 'gray-matter';
+
+export default function Teams({ teams }) {
 	return (
-		<section>
-			<div></div>
+		<section className='mt-40'>
+			<Flex classes='mx-auto justify-between'>
+				<div className='lg:w-1/2 mr-5'>
+					<h2>Our Teams</h2>
+					<p>We are a group of under 25s coming from every corner of Europe.</p>
+					<p className='my-5'>
+						Our association was born from our wish to value real and deep ties,
+						horizontality and dialogue, curiosity and welcoming of the
+						different. After lots of videocalls, thousand doubts and just as
+						many ideas, our team has become a family. We are proud of our
+						diversity and passion, enthusiastic in sharing inspiration and in
+						continuously proposing new perspectives.{' '}
+					</p>
+					<p>
+						We will be waiting for you in Sanremo, to introduce ourselves
+						properly. In the meanwhile, here is an anticipation of who we are.
+					</p>
+					<Button classes='btn-primary mt-3'>Join our team</Button>
+				</div>
+				<Image src='/illustrations/group.png' alt='' width={600} height={400} />
+			</Flex>
+			<div className='grid grid-cols-1 lg:px-16 gap-20 lg:grid-cols-3'>
+				{teams.map(({ team, frontmatter, content }) => (
+					<TeamCard
+						key={team}
+						imgSrc={frontmatter.image}
+						teamName={frontmatter.name}
+						duty={frontmatter.duty}
+						desc={content}
+						path={team}
+					/>
+				))}
+			</div>
 		</section>
 	);
+}
+
+export async function getStaticProps() {
+	const files = getAllTeams();
+	const teams = files.map((team) => {
+		//const readFile = fs.readFileSync(`teams/${team}/${team}.md`, 'utf-8');
+		const readFile = getATeam(team);
+		const { data, content } = matter(readFile);
+		return {
+			team,
+			frontmatter: data,
+			content,
+		};
+	});
+
+	return {
+		props: {
+			teams,
+		},
+	};
 }
