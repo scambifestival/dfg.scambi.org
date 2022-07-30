4d6bf4bdcdcdeca4af5b7d0224500d4fb11b98f5
traciingu <traciinguyen@hotmail.com>
2022-07-29 23:24:43 -0400

    Navbar responsive to desktop & Galaxy fold screens

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -52,7 +52,7 @@ export default function Navbar() {
 			ref={navbar}
 			onBlur={clickOutside}
 			className={`${isOpen ? 'rounded-3xl' : 'rounded-full'
-				} fixed w-11/12 top-10 left-1/2 -translate-x-1/2 z-50 flex flex-col items-start justify-between bg-white shadow-xl py-5 px-10 space-y-7 sm:px-14 lg:flex-row lg:items-center lg:space-y-0`}>
+				} fixed w-11/12 top-10 left-1/2 -translate-x-1/2 z-50 flex flex-col items-start justify-between bg-white shadow-xl py-5 px-10 space-y-7 sm:px-14 lg:flex-row lg:items-center lg:space-y-0 xl:px-20`}>
 			<div className='w-full flex justify-between items-center lg:w-auto lg:px-0'>
 				<button
 					type='button'
@@ -64,29 +64,30 @@ export default function Navbar() {
 				<Link href='/'>
 					<a
 						id='logo'
-						className=' font-medium text-4xl outline-none hover:text-fuchsia-800 active:text-fuchsia-800 focus:text-fuchsia-800'>
+						className='font-semibold text-3xl iPhoneSE:text-4xl outline-none hover:text-fuchsia-800 active:text-fuchsia-800 focus:text-fuchsia-800'>
 						Scambi
 					</a>
 				</Link>
 				<Dropdown
-					name={router.locale.toUpperCase()}
+					name={router.locale}
 					content={languageDropdown}
-					dropdownStyle='w-fit px-2 absolute left-auto right-0 shadow-xl'
+					btnStyle='text-xl uppercase lg:hidden'
+					dropdownStyle='absolute w-fit px-2 left-auto right-0 shadow-xl'
 				/>
 			</div>
 			<ul
 				role='menubar'
 				aria-label='scambi'
 				className={`${isOpen ? 'flex' : 'hidden'
-					} w-full space-y-3 flex-col justify-between items-start space-x-0 lg:w-auto lg:flex lg:flex-row lg:items-center lg:space-y-0 xl:space-x-10 2xl:w-auto 2xl:space-x-16`}>
+					} w-full space-y-3 flex-col justify-between items-start space-x-0 lg:w-auto lg:flex lg:flex-row lg:items-center lg:space-x-6 lg:space-y-0 xl:space-x-16`}>
 				<Dropdown
 					name='About us'
-					btnStyle='text-xl 2xl:text-2xl font-bold uppercase'
+					btnStyle='text-xl font-bold uppercase'
 					content={aboutUsDropdown}
 				/>
 				<Dropdown
 					name='Festival'
-					btnStyle='text-xl 2xl:text-2xl font-bold uppercase'
+					btnStyle='text-xl font-bold uppercase'
 					content={festivalDropdown}
 				/>
 				<Dropdown
@@ -96,18 +97,25 @@ export default function Navbar() {
 				/>
 				<li role='none'>
 					<Link href='/faq'>
-						<a className='text-xl 2xl:text-2xl font-bold outline-none hover:text-fuchsia-800 active:text-fuchsia-800 focus:text-fuchsia-800'>
+						<a className='text-xl font-bold outline-none hover:text-fuchsia-800 active:text-fuchsia-800 focus:text-fuchsia-800'>
 							FAQ
 						</a>
 					</Link>
 				</li>
 			</ul>
 
-			<Button
-				classes={`${isOpen ? '' : 'hidden'
-					} lg:inline-block uppercase outline-offset-1 btn-primary`}>
-				Attend
-			</Button>
+			<div className={`${isOpen ? '' : 'hidden'} lg:inline-block lg:w-fit lg:space-x-5 xl:space-x-10`}>
+				<Button
+					classes={`lg:inline-block uppercase outline-offset-1 btn-primary`}>
+					Attend
+				</Button>
+				<Dropdown
+					name={router.locale}
+					content={languageDropdown}
+					btnStyle='text-xl uppercase hidden lg:inline-flex'
+					dropdownStyle='w-fit px-2 left-auto right-0 shadow-xl'
+				/>
+			</div>
 		</nav>
 	);
 }
