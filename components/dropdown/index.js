678baf8dd70006d075bc518ab73f3254bab66947
traciingu <traciinguyen@hotmail.com>
2022-08-01 15:41:15 -0400

    Add accessible tags to navbar

--- components/dropdown/index.js
+++ components/dropdown/index.js
@@ -27,10 +27,11 @@ export default function Dropdown({ btnStyle, dropdownStyle, name, content }) {
 	return (
 		<div className='relative inline-block' onBlur={clickOutside}>
 			<button
+				aria-haspopup='true'
+				aria-expanded={isOpen}
 				onClick={handleClick}
-				className={`${btnStyle} ${
-					isOpen ? 'text-fuchsia-800' : ''
-				} inline-flex justify-start items-center shadow-none cursor-pointer px-0 py-2 bg-white outline-none hover:text-fuchsia-800 active:text-fuchsia-800 focus:text-fuchsia-800`}>
+				className={`${btnStyle} ${isOpen ? 'text-fuchsia-800' : ''
+					} inline-flex justify-start items-center shadow-none cursor-pointer px-0 py-2 bg-white outline-none hover:text-fuchsia-800 active:text-fuchsia-800 focus:text-fuchsia-800`}>
 				{name}
 				<span className='pl-2' aria-hidden='true'>
 					{isOpen ? <ChevronUp /> : <ChevronDown />}
@@ -38,18 +39,20 @@ export default function Dropdown({ btnStyle, dropdownStyle, name, content }) {
 			</button>
 			<ul
 				ref={menu}
-				className={`${dropdownStyle} ${
-					isOpen ? '' : 'hidden'
-				} py-1 left-0 mt-2 w-56 rounded-md bg-white lg:absolute lg:shadow-xl`}
+				className={`${dropdownStyle} ${isOpen ? '' : 'hidden'
+					} py-1 left-0 mt-2 w-56 rounded-md bg-white lg:absolute lg:shadow-xl`}
 				id={name}
 				role='menu'
 				aria-label={name}
 				aria-orientation='vertical'
 				aria-labelledby='menu-button'>
 				{content.map((child, index) => (
-					<li key={index} role='menuitem'>
+					<li key={index} role='none'>
 						<Link href={child.href} locale={child.locale || router.locale}>
-							<a className='text-lg font-bold block px-4 py-2 uppercase outline-none hover:text-fuchsia-800 active:text-fuchsia-800 focus:text-fuchsia-800'>
+							<a
+								role='menuitem'
+								className='text-lg font-bold block px-4 py-2 uppercase outline-none hover:text-fuchsia-800 active:text-fuchsia-800 focus:text-fuchsia-800'
+							>
 								{child.title}
 							</a>
 						</Link>
