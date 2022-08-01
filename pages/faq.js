13ee65b5cb39c96508bd4c6c59e30608a89313e7
traciingu <traciinguyen@hotmail.com>
2022-07-31 23:48:21 -0400

    Add responsive css for all screens

--- pages/faq.js
+++ pages/faq.js
@@ -7,15 +7,15 @@ import { data } from '../assets/data';
 
 export default function FAQ() {
 	return (
-		<Flex classes='my-32 w-full mx-auto items-center'>
-			<div className='flex flex-col items-center mx-auto lg:mr-20'>
-				<h2 className='text-center lg:mb-5'>Frequently Asked Questions</h2>
+		<Flex classes='my-32 w-full mx-auto items-center md:flex-col lg:flex-row'>
+			<div className='flex flex-col items-center self-start mx-auto iPhoneSE:mx-4 sm:mx-12 lg:mx-0 lg:mr-24'>
+				<h2 className='text-center lg:mb-10'>Frequently Asked Questions</h2>
 				<div className='hidden lg:block'>
-					<Image src='/faq.png' alt='' width={660} height={500} />
+					<Image src='/faq.png' alt='' className='rounded-xl' width={660} height={500} />
 				</div>
 			</div>
 			<div className='mx-auto w-full'>
-				<div className='flex justify-center'>
+				<div className='flex justify-center mx-1 iPhoneSE:mx-3 sm:mx-6 lg:mx-0'>
 					<Accordion content={data} />
 				</div>
 			</div>
