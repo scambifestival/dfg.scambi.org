c6a2049a6e26f145c0db101a4455a2c382479af5
traciingu <traciinguyen@hotmail.com>
2022-08-07 18:21:06 -0400

    Add placeholder blob svg

--- pages/labs/[id].js
+++ pages/labs/[id].js
@@ -20,12 +20,40 @@ export default function Lab({ lab }) {
 					<p>{getDescription(locale, lab)}</p>
 				</div>
 
-				<Image
+				{/* <Image
 					src='https://picsum.photos/400'
 					alt=''
 					width={387}
 					height={427}
+				/> */}
+				<div className='mb-5 mx-auto w-full min-w-0 iPhoneXR:w-2/3 sm:w-1/2 lg:order-last lg:mb-0 lg:w-1/3'>
+					<svg
+						viewBox='0 0 282 258'
+						fill='none'
+						xmlns='http://www.w3.org/2000/svg'>
+						<defs>
+							<pattern
+								id='img'
+								patternUnits='userSpaceOnUse'
+								width='282'
+								height='258'>
+								<image
+									href='https://picsum.photos/282/258'
+									x='0'
+									y='0'
+									width='282'
+									height='258'
 				/>
+							</pattern>
+						</defs>
+						<path
+							fillRule='evenodd'
+							clipRule='evenodd'
+							d='M170.442 11.7096C204.381 18.8402 242.989 19.868 264.504 46.3403C286.873 73.8645 283.241 110.797 278.205 144.303C272.884 179.703 265.768 217.594 235.669 238.188C204.579 259.459 160.798 264.066 124.08 249.752C91.019 236.863 83.1051 198.397 62.2472 170.58C40.1257 141.078 -1.99244 119.718 0.0732747 84.3713C2.21638 47.7003 37.1047 20.6977 71.8731 5.86908C102.524 -7.20361 136.807 4.64308 170.442 11.7096Z'
+							fill='url(#img)'
+						/>
+					</svg>
+				</div>
 			</Flex>
 			<Flex classes='mt-10 mx-auto justify-between bg-white'>
 				<div className='text-left lg:w-1/2'>
@@ -34,12 +62,41 @@ export default function Lab({ lab }) {
 						<p key={host.id}>{host.value}</p>
 					))}
 				</div>
-				<Image
+				{/* <Image
 					src='https://picsum.photos/400'
 					alt=''
 					width={387}
 					height={427}
+				/> */}
+
+				<div className='mb-5 mx-auto w-full min-w-0 iPhoneXR:w-2/3 sm:w-1/2 lg:order-last lg:mb-0 lg:w-1/3'>
+					<svg
+						viewBox='0 0 282 258'
+						fill='none'
+						xmlns='http://www.w3.org/2000/svg'>
+						<defs>
+							<pattern
+								id='img'
+								patternUnits='userSpaceOnUse'
+								width='282'
+								height='258'>
+								<image
+									href='https://picsum.photos/282/258'
+									x='0'
+									y='0'
+									width='282'
+									height='258'
 				/>
+							</pattern>
+						</defs>
+						<path
+							fillRule='evenodd'
+							clipRule='evenodd'
+							d='M170.442 11.7096C204.381 18.8402 242.989 19.868 264.504 46.3403C286.873 73.8645 283.241 110.797 278.205 144.303C272.884 179.703 265.768 217.594 235.669 238.188C204.579 259.459 160.798 264.066 124.08 249.752C91.019 236.863 83.1051 198.397 62.2472 170.58C40.1257 141.078 -1.99244 119.718 0.0732747 84.3713C2.21638 47.7003 37.1047 20.6977 71.8731 5.86908C102.524 -7.20361 136.807 4.64308 170.442 11.7096Z'
+							fill='url(#img)'
+						/>
+					</svg>
+				</div>
 			</Flex>
 			{/* <div id='similar-lab' className='py-10 px-5 iPhoneXR:px-10 md:px-5 lg:px-10'>
 				<h2 className='mb-10 text-left font-semibold leading-tight'>Similar Labs</h2>
