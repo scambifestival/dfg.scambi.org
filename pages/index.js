3e644ed6c2e84f6474ebfba4e389fa4503b633c3
Uyen Hoang <minhuyen0512@gmail.com>
2022-08-06 17:43:35 -0700

    Add error component for Teams page for Italian

--- pages/index.js
+++ pages/index.js
@@ -25,8 +25,12 @@ export default function Home() {
 			<header className='space-y-7'>
 				<div className='w-full flex flex-col justify-between mt-10 text-center sm:w-10/12 sm:mx-auto sm:my-10 sm:text-left xl:flex-row xl:items-end'>
 					<div className='mx-3 my-5 space-y-5 sm:mx-0 md:space-y-2 md:my-3 xl:my-0'>
-						<h1 className='leading-none font-medium text-[5.4rem] iPhoneSE:text-[102px]'>Scambi Festival</h1>
-						<h3 className='leading-tight font-medium'>The Festival of Paneuretic Workshops, free for all to attend</h3>
+						<h1 className='leading-none font-medium text-[5.4rem] iPhoneSE:text-[102px]'>
+							Scambi Festival
+						</h1>
+						<h3 className='leading-tight font-medium'>
+							The Festival of Paneuretic Workshops, free for all to attend
+						</h3>
 					</div>
 					<p className='font-medium'>
 						<span className='text-2xl'>August 25 - 28, 2022</span> <br />
@@ -38,7 +42,7 @@ export default function Home() {
 						className='absolute top-0 left-0 w-full h-full'
 						src='https://player.vimeo.com/video/658183199'
 						title='Scambi Festival 2021 Rewind'
-						allow="autoplay; fullscreen"
+						allow='autoplay; fullscreen'
 					/>
 				</div>
 			</header>
@@ -68,8 +72,7 @@ export default function Home() {
 
 			<Flex
 				classes='mt-10 mb-32 mx-7 py-0 justify-between text-center sm:mx-16 md:mx-auto md:mt-20 md:text-left md:space-x-reverse md:space-x-20 lg:space-x-reverse lg:space-x-28 2xl:mt-10 2xl:w-9/12'
-				reverse={true}
-			>
+				reverse={true}>
 				<div className='space-y-3 sm:space-y-6 md:w-full'>
 					<h2 className='leading-tight font-medium text-[3.5rem] iPhoneSE:text-[64px]'>
 						2022 Edition: “Imbalance”
@@ -109,7 +112,7 @@ export default function Home() {
 					<Card
 						key={index}
 						classes='w-5/6 h-fit px-7 py-5 space-y-4 text-left bg-white lg:w-1/4'>
-						<h4 class='font-["Poppins"] font-medium'>{event.title}</h4>
+						<h4 className='font-["Poppins"] font-medium'>{event.title}</h4>
 						<p>{event.description}</p>
 						<div>
 							<Link href={`/${event.link}`}>
@@ -124,7 +127,9 @@ export default function Home() {
 			</div>
 
 			<div className='mt-28 lg:px-16 text-center'>
-				<h2 className='mb-10 font-medium leading-tight'>Scambi 2021 Highlights</h2>
+				<h2 className='mb-10 font-medium leading-tight'>
+					Scambi 2021 Highlights
+				</h2>
 				<Carousel slides={highlights} />
 			</div>
 
@@ -179,10 +184,10 @@ export default function Home() {
 
 			<Flex classes='mx-auto my-10 space-y-4 text-center md:flex-col lg:flex-row lg:space-x-10 lg:text-left'>
 				<div className='mx-4 space-y-7 lg:w-3/5'>
-					<h2 className='leading-tight lg:leading-normal font-medium'>Scambi Family</h2>
-					<p>
-						We are a group of under 25s coming from every corner of Europe.
-					</p>
+					<h2 className='leading-tight lg:leading-normal font-medium'>
+						Scambi Family
+					</h2>
+					<p>We are a group of under 25s coming from every corner of Europe.</p>
 					<p>
 						Our association was born from our wish to value real and deep ties,
 						horizontality and dialogue, curiosity and welcoming of the
@@ -215,16 +220,11 @@ export default function Home() {
 			<div className='text-center px-7 py-10 font-normal bg-white sm:px-14 xl:my-36'>
 				<p className='text-2xl sm:leading-relaxed sm:text-3xl lg:leading-loose lg:text-4xl xl:text-5xl xl:leading-loose'>
 					“Our purpose is to recover the value of{' '}
-					<span className='font-bold'>
-						communication
-					</span>{' '}
-					and{' '}
-					<span className='font-bold'>
-						meeting
-					</span>
-					, often forgotten or hidden behind a two-dimentional screen. Scambi is
-					a multi-dimensional universe, where distant “planets” have the
-					possibility to interact with each other.”
+					<span className='font-bold'>communication</span> and{' '}
+					<span className='font-bold'>meeting</span>, often forgotten or hidden
+					behind a two-dimentional screen. Scambi is a multi-dimensional
+					universe, where distant “planets” have the possibility to interact
+					with each other.”
 				</p>
 			</div>
 
@@ -233,11 +233,16 @@ export default function Home() {
 				<div className='flex flex-wrap justify-center items-stretch mx-5 space-y-6 sm:mx-16 md:mx-5 md:space-y-0'>
 					{manifestoCards.map((card, index) => (
 						<Card
-							classes={`p-5 w-full h-fit md:max-w-[40%] md:mr-7 md:mb-24 xl:w-1/5 ${index % 2 === 0 ? 'yellow-pink-gradient' : 'bg-white'
-								}`}
+							classes={`p-5 w-full h-fit md:max-w-[40%] md:mr-7 md:mb-24 xl:w-1/5 ${
+								index % 2 === 0 ? 'yellow-pink-gradient' : 'bg-white'
+							}`}
 							key={`manifesto-${index}`}>
-							<h3 className='leading-loose text-center font-["Poppins"] font-semibold text-2xl'>{card.title}</h3>
-							<p className='leading-relaxed text-center sm:text-left'>{card.description}</p>
+							<h3 className='leading-loose text-center font-["Poppins"] font-semibold text-2xl'>
+								{card.title}
+							</h3>
+							<p className='leading-relaxed text-center sm:text-left'>
+								{card.description}
+							</p>
 						</Card>
 					))}
 				</div>
@@ -254,7 +259,9 @@ export default function Home() {
 				classes='my-1 mx-4 space-y-10 sm:mx-10 md:my-20 lg:mx-auto md:space-x-reverse md:space-x-16 xl:space-x-reverse xl:space-x-40'
 				reverse={true}>
 				<div className='w-full flex flex-col text-center space-y-8 md:text-left'>
-					<h2 className='leading-tight lg:leading-normal font-medium'>Our Relations</h2>
+					<h2 className='leading-tight lg:leading-normal font-medium'>
+						Our Relations
+					</h2>
 					<p>
 						Alone we would not have been able to concretize the magic of Scambi.
 						What makes our festival unique are the relationships and the bonds
@@ -280,7 +287,9 @@ export default function Home() {
 			</Flex>
 
 			<div className='m-7 sm:m-10 xl:mx-20'>
-				<h2 className='leading-tight font-medium text-center sm:text-left py-5'>Explore Scambi Festival</h2>
+				<h2 className='leading-tight font-medium text-center sm:text-left py-5'>
+					Explore Scambi Festival
+				</h2>
 				<Map />
 			</div>
 		</section>
