628dfca96d073530787c578abb585d88b6380ad3
Rion Sawabe <rose1029ri@yahoo.co.jp>
2022-08-09 09:53:58 -0700

    fix linter error

--- components/map/index.js
+++ components/map/index.js
@@ -1,14 +1,12 @@
 export default function Map() {
 	return (
-		<div className='shadow-2xl rounded-xl overflow-hidden'>
+		<div className="shadow-2xl rounded-xl overflow-hidden">
 			<iframe
 				width="100%"
 				height="400px"
-				frameborder="0"
-				allowfullscreen
-				src="//umap.openstreetmap.fr/en/map/scambi-2022_792002?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=true&allowEdit=false&moreControl=true&searchControl=true&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false"
-			>
-			</iframe>
+				frameBorder="0"
+				allowFullScreen
+				src="//umap.openstreetmap.fr/en/map/scambi-2022_792002?scaleControl=false&miniMap=false&scrollWheelZoom=true&zoomControl=true&allowEdit=false&moreControl=true&searchControl=true&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false"></iframe>
 		</div>
 	);
 }
