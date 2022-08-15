48339decd0333e5458dec93b23327c21bda845b3
Rion Sawabe <rose1029ri@yahoo.co.jp>
2022-08-15 14:10:03 -0700

    blog card tag & categories

--- components/card/blog-card/index.js
+++ components/card/blog-card/index.js
@@ -1,41 +1,56 @@
-import Card from '..';
-import Image from 'next/image';
-import Link from 'next/link';
-import { getDate } from '../../../lib/blog';
+import Card from "..";
+import Image from "next/image";
+import Link from "next/link";
+import { getDate } from "../../../lib/blog";
 
 export default function BlogCard({ post, path }) {
 	return (
 		<Link
 			href={`blog/${path}`}
 			as={path}
-			locale={'lang' in post ? post.lang : ''}>
+			locale={"lang" in post ? post.lang : ""}>
 			<a>
-				<Card classes='bg-white w-96 m-12 drop-shadow-2xl'>
+				<Card classes="bg-white iPhoneXR:w-96 SurfaceDuo:m-12 drop-shadow-2xl GalaxyS8:w-80 GalaxyFold:mb-12 GalaxyFold:w-66 md:w-80">
 					<Image
 						src={post.image}
-						alt=''
+						alt=""
+						layout="responsive"
 						height={316}
 						width={420}
-						className='rounded-t-2xl w-full h-56'
+						className="rounded-t-2xl h-56 w-fit GalaxyFold:w-66"
 					/>
 
-					<div className='flex justify-between mt-6 pb-6 px-6'>
+					<div className="flex justify-between mt-6 pb-6 px-6">
 						<p>
 							By <span>{post.author}</span>
 						</p>
 						<p>{getDate(new Date(post.date))}</p>
 					</div>
 
-					<div className='text-left px-6 w-full'>
-						<h4 className='pb-4'>{post.title}</h4>
-						<div className='pb-5 flex space-x-2'>
+					<div className="text-left px-6 w-full">
+						<h4 className="pb-4">{post.title}</h4>
+						<div className="pb-5 flex space-x-2">
 							{post.categories.map((el) => (
 								<p
 									key={el}
-									className='bg-yellow-300 w-fit px-2 py-1 rounded-md'>
+									className={
+										(category_colors[el] || default_category_color) +
+										" w-fit px-2 py-1 rounded-md"
+									}>
 									{el}
 								</p>
 							))}
+						</div>
+
+						<div className="pb-5 flex space-x-2">
+							{post.tags?.slice(0, 2).map((el) => (
+								<span key={el} className="text-gray-400  text-left pb-4">
+									#{el}
+								</span>
+							))}
+							{post.tags?.length > 2 ? (
+								<span className="text-gray-400  text-left pb-4">...</span>
+							) : null}
 						</div>
 					</div>
 				</Card>
@@ -43,3 +58,12 @@ export default function BlogCard({ post, path }) {
 		</Link>
 	);
 }
+
+const category_colors = {
+	thinking: "bg-yellow-300",
+	cartoline: "bg-pink-300",
+	SquiLibristi: "bg-orange-400",
+	GDL: "bg-green-400"
+};
+
+const default_category_color = "bg-blue-400";
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,97 +1,79 @@
-import Subscribe from '../../components/subscribe';
-import React, { useState } from 'react';
-import Button from '../../components/button';
-import BlogCard from '../../components/card/blog-card';
-import { getAPost, getAllPosts, sortByDate } from '../../lib/blog';
-import matter from 'gray-matter';
-import Image from 'next/image';
+import Subscribe from "../../components/subscribe";
+import React, { useEffect, useState } from "react";
+import Button from "../../components/button";
+import BlogCard from "../../components/card/blog-card";
+import { getAPost, getAllPosts, sortByDate } from "../../lib/blog";
+import matter from "gray-matter";
+import Image from "next/image";
 
-export default function Blog({ posts }) {
-	const [openedTab, setOpenedTab] = useState(1);
+export default function Blog({ posts, categories }) {
+	const [filter, setFilter] = useState("");
+	const [filteredPosts, setFilteredPosts] = useState(posts);
+	const [postNumber, setPostNumber] = useState(6);
+	function handleClick() {
+		setPostNumber((prevPostNumber) => prevPostNumber + 5);
+	}
 
+	useEffect(() => {
+		if (filter === "") {
+			setFilteredPosts(posts);
+		} else {
+			setFilteredPosts(
+				posts.filter((post) => post.frontmatter.categories.includes(filter))
+			);
+		}
+	}, [filter]);
 	return (
-		<section className='bg-white -mb-12'>
-			<div className='relative w-screen -mt-48'>
-				<div className='w-full h-96 flex justify-center'>
-					<h2 className='z-20 text-white text-center mt-36 font-bold md:w-1/3 sm:w-1/2 SurfaceDuo:w-1/3 sm:mt-44'>
+		<section className="bg-white -mb-12">
+			<div className="relative w-screen -mt-48">
+				<div className="w-full h-96 flex justify-center">
+					<h2 className="z-20 text-white text-center mt-36 font-bold md:w-1/3 sm:w-1/2 SurfaceDuo:w-1/3 sm:mt-44">
 						Blog
 					</h2>
 				</div>
 
 				<Image
-					src='/illustrations/workshop-explanation.png'
-					alt=''
-					layout='fill'
-					className='object-cover w-full absolute top-0 left-0 GalaxyFold:h-72 sm:h-96'
+					src="https://x.scambi.org/illustrations/paneuretico.webp"
+					alt=""
+					layout="fill"
+					className="object-cover w-full absolute top-0 left-0 GalaxyFold:h-64 h-36"
 				/>
 			</div>
-			<div className='pb-36'>
-				{/*<div className=' flex justify-around sm:mt-24 mb-24 h-12'>
-					<ul className='flex flex-wrap text-center justify-center'>
+			<div className="pb-36">
+				<div className=" flex justify-around GalaxyFold:mt-24 mb-24 h-12">
+					<ul className="flex flex-wrap text-center justify-center">
 						<li
 							onClick={(e) => {
 								e.preventDefault();
-								setOpenedTab(1);
+								setFilter("");
 							}}
 							className={
-								'mr-2  border-b-2 hover:border-b-4 cursor-pointer SurfaceDuo:mr-6 SurfaceDuo:ml-6 GalaxyFold:mr-3 iPhoneSE:mr-4 iPhoneSE:ml-4 ' +
-								(openedTab === 1 ? 'border-purple-800 border-b-4' : '')
+								"mr-2  border-b-2 hover:border-b-4 cursor-pointer SurfaceDuo:mr-6 SurfaceDuo:ml-6 GalaxyFold:mr-2 iPhoneSE:mr-4 iPhoneSE:ml-4 " +
+								(filter === "" ? "border-purple-800 border-b-4" : "")
 							}>
-							<p className={openedTab === 1 ? 'font-bold' : ''}>All Posts</p>
+							<p className={filter === "" ? "font-bold" : ""}>All Posts</p>
 						</li>
-						<li
-							onClick={(e) => {
-								e.preventDefault();
-								setOpenedTab(2);
-							}}
-							className={
-								'mr-2  border-b-2 hover:border-b-4 cursor-pointer SurfaceDuo:mr-6 SurfaceDuo:ml-6 GalaxyFold:mr-2 iPhoneSE:mr-4 iPhoneSE:ml-4 ' +
-								(openedTab === 2 ? 'border-purple-800 border-b-4' : '')
-							}>
-							<p className={openedTab === 2 ? 'font-bold' : ''}>
-								Trade in the room
-							</p>
-						</li>
-						<li
-							onClick={(e) => {
-								e.preventDefault();
-								setOpenedTab(3);
-							}}
-							className={
-								'mr-2  border-b-2 cursor-pointer SurfaceDuo:mr-6 SurfaceDuo:ml-6 GalaxyFold:mr-2 iPhoneSE:mr-4 iPhoneSE:ml-4 ' +
-								(openedTab === 3 ? 'border-purple-800 border-b-4' : '')
-							}>
-							<p className={openedTab === 3 ? 'font-bold' : ''}>Postcards</p>
-						</li>
-						<li
-							onClick={(e) => {
-								e.preventDefault();
-								setOpenedTab(4);
-							}}
-							className={
-								'mr-2  border-b-2 cursor-pointer SurfaceDuo:mr-6 SurfaceDuo:ml-6 GalaxyFold:mr-2 iPhoneSE:mr-4 iPhoneSE:ml-4 ' +
-								(openedTab === 4 ? 'border-purple-800 border-b-4' : '')
-							}>
-							<p className={openedTab === 4 ? 'font-bold' : ''}>Category 3</p>
-						</li>
-						<li
-							onClick={(e) => {
-								e.preventDefault();
-								setOpenedTab(5);
-							}}
-							className={
-								'mr-2  border-b-2 cursor-pointer SurfaceDuo:mr-6 SurfaceDuo:ml-6 GalaxyFold:mr-2 iPhoneSE:mr-4 iPhoneSE:ml-4 ' +
-								(openedTab === 5 ? 'border-purple-800 border-b-4' : '')
-							}>
-							<p className={openedTab === 5 ? 'font-bold' : ''}>
-								Uncategorized
-							</p>
-						</li>
+						{categories.map((category) => (
+							<li
+								key={"category-" + category}
+								onClick={(e) => {
+									e.preventDefault();
+									setFilter(category);
+								}}
+								className={
+									"border-b-2 hover:border-b-4 cursor-pointer SurfaceDuo:mr-6 SurfaceDuo:ml-6 GalaxyFold:mr-3 GalaxyFold:ml-3 iPhoneSE:mr-4 iPhoneSE:ml-4 " +
+									(filter === category ? "border-purple-800 border-b-4" : "")
+								}>
+								<p className={filter === category ? "font-bold" : ""}>
+									{category}
+								</p>
+							</li>
+						))}
 					</ul>
-						</div>*/}
+				</div>
 				<div>
-					<div className='flex flex-wrap mx-auto justify-center'>
-						{posts.map(({ frontmatter, path }) => (
+					<div className="flex flex-wrap mx-auto justify-center">
+						{filteredPosts.slice(0, postNumber).map(({ frontmatter, path }) => (
 							<BlogCard
 								key={frontmatter.title}
 								post={frontmatter}
@@ -99,13 +81,23 @@ export default function Blog({ posts }) {
 							/>
 						))}
 					</div>
-					<div className='flex justify-center'>
-						<Button classes='btn-primary SurfaceDuo:mr-0 iPhoneSE:px-12 iPhoneSE:h-auto GalaxyFold:mt-12'>
-							<a href=''>Load more</a>
-						</Button>
-					</div>
+					{!(postNumber >= filteredPosts.length) ? (
+						!(filteredPosts.length <= 6) ? (
+							<div className="flex justify-center">
+								<Button
+									classes="btn-primary SurfaceDuo:mr-0 iPhoneSE:px-12 iPhoneSE:h-auto GalaxyFold:mt-12"
+									onClick={handleClick}>
+									Load more
+								</Button>
+							</div>
+						) : (
+							<div></div>
+						)
+					) : (
+						<div></div>
+					)}
 				</div>
-				<div className='bg-white flex justify-center py-10'>
+				<div className="bg-white flex justify-center py-10">
 					<Subscribe />
 				</div>
 			</div>
@@ -115,6 +107,7 @@ export default function Blog({ posts }) {
 
 export async function getStaticProps() {
 	const files = getAllPosts();
+	const categories = new Set();
 	let posts = files.map((post) => {
 		const readFile = getAPost(post);
 		let data = null;
@@ -122,20 +115,21 @@ export async function getStaticProps() {
 			const parse = matter(readFile);
 			data = parse.data;
 			data.date = new Date(data.date).toLocaleDateString();
+			data.categories.forEach((category) => categories.add(category));
 		}
-		post = post.replace('.md', '');
-
+		post = post.replace(".md", "");
+		console.log(data);
 		return {
 			path: post,
-			frontmatter: data,
+			frontmatter: data
 		};
 	});
 
 	posts.sort(sortByDate);
-
 	return {
 		props: {
 			posts: posts,
-		},
+			categories: Array.from(categories)
+		}
 	};
 }
